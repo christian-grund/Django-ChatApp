0e7109227a5f09fa072aa763876aef63bd96bc35
christian-grund <151537557+christian-grund@users.noreply.github.com>
2024-07-09 15:21:49 +0200

    09 - Datenbank Objekt erstellen

--- chat/admin.py
+++ chat/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from .models import Message
+from .models import Chat, Message
 
 class MessageAdmin(admin.ModelAdmin):
-    fields = ('text', 'created_at', 'author', 'receiver')           # Ansicht der Felder, die in der Detailansicht der Message angezeigt werden (z.B. nur für bestimmte Admins sichtbar)
+    fields = ('chat', 'text', 'created_at', 'author', 'receiver')           # Ansicht der Felder, die in der Detailansicht der Message angezeigt werden (z.B. nur für bestimmte Admins sichtbar)
     list_display = ('text', 'author', 'receiver', 'created_at')     # Anzeige der Spalten in der Chat-Übersicht
     search_fields = ('text',)                                       # Zum Filtern nach einer Kategorie
 
 # Register your models here.
 admin.site.register(Message, MessageAdmin)
+admin.site.register(Chat)
--- chat/views.py
+++ chat/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import Chat, Message
 
 # request: Objekt, wird standardmäßig in diese Funktion reingegeben
 def index(request):
+    if request.method == 'POST':
+        print("Received data: " + request.POST['textmessage'])
+        myChat = Chat.objects.get(id=1)
+        Message.objects.create(text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)                              # Nachricht wird erstellt
     return render(request, 'chat/index.html', {'username': 'Christian'})
 
