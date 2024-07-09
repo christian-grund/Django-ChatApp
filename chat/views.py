d1cdb46fb7002a519836d9d4442ecd36afd3b265
christian-grund <151537557+christian-grund@users.noreply.github.com>
2024-07-09 16:00:16 +0200

    11 - Authentication

--- chat/views.py
+++ chat/views.py
@@ -6,6 +6,7 @@ def index(request):
     if request.method == 'POST':
         print("Received data: " + request.POST['textmessage'])
         myChat = Chat.objects.get(id=1)
-        Message.objects.create(text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)                              # Nachricht wird erstellt
-    return render(request, 'chat/index.html', {'username': 'Christian'})
+        Message.objects.create(text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)  
+    chatMessages = Message.objects.filter(chat__id=1)                            # Nachricht wird erstellt
+    return render(request, 'chat/index.html', {'messages': chatMessages})
 
