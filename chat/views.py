ea28cfe2015452e2b06afbfeaf1b669171591f7f
christian-grund <151537557+christian-grund@users.noreply.github.com>
2024-07-09 16:55:58 +0200

    12 - Login-View

--- chat/views.py
+++ chat/views.py
@@ -1,12 +1,25 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from .models import Chat, Message
+from django.contrib.auth import authenticate, login
+
 
 # request: Objekt, wird standardmäßig in diese Funktion reingegeben
 def index(request):
     if request.method == 'POST':
         print("Received data: " + request.POST['textmessage'])
         myChat = Chat.objects.get(id=1)
-        Message.objects.create(text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)  
-    chatMessages = Message.objects.filter(chat__id=1)                            # Nachricht wird erstellt
+        Message.objects.create(text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)  # Nachricht wird erstellt
+    chatMessages = Message.objects.filter(chat__id=1)                            
     return render(request, 'chat/index.html', {'messages': chatMessages})
 
+def login_view(request):
+    if request.method == 'POST':
+        user = authenticate(username=request.POST.get('username'), password=request.POST.get('password'))
+
+        if user:
+            login(request, user)
+            return HttpResponseRedirect('/chat/')
+        else:
+            return render(request, 'auth/login.html', {'wrongPassword': True})
+    return render(request, 'auth/login.html')
