58efceaa5de2f61501838c7265747212f3bfa07b
christian-grund <151537557+christian-grund@users.noreply.github.com>
2024-07-10 14:54:45 +0200

    16 - Aufgabe Registrierung

--- chat/views.py
+++ chat/views.py
@@ -1,29 +1,53 @@
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Chat, Message
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from .forms import RegisterForm
+
 
 
 # request: Objekt, wird standardmäßig in diese Funktion reingegeben
 @login_required(login_url='/login/')
 def index(request):
-    if request.method == 'POST':
-        print("Received data: " + request.POST['textmessage'])
-        myChat = Chat.objects.get(id=1)
-        Message.objects.create(text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)  # Nachricht wird erstellt
-    chatMessages = Message.objects.filter(chat__id=1)                            
-    return render(request, 'chat/index.html', {'messages': chatMessages})
+	if request.method == 'POST':
+		print("Received data: " + request.POST['textmessage'])
+		myChat = Chat.objects.get(id=1)
+		Message.objects.create(text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)  # Nachricht wird erstellt
+	chatMessages = Message.objects.filter(chat__id=1)                            
+	return render(request, 'chat/index.html', {'messages': chatMessages})
 
 def login_view(request):
-    redirect = request.GET.get('next')
-    print(f"Redirect parameter: {redirect}")  # Debug-Ausgabe
-    if request.method == 'POST':
-        user = authenticate(username=request.POST.get('username'), password=request.POST.get('password'))
+	redirect = request.GET.get('next')
+	print(f"Redirect parameter: {redirect}")  # Debug-Ausgabe
+	if request.method == 'POST':
+		user = authenticate(username=request.POST.get('username'), password=request.POST.get('password'))
 
-        if user:
-            login(request, user)
-            return HttpResponseRedirect(request.POST.get('redirect'))
-        else:
-            return render(request, 'auth/login.html', {'wrongPassword': True, 'redirect': redirect})
-    return render(request, 'auth/login.html', {'redirect': redirect})
+		if user:
+			login(request, user)
+			return HttpResponseRedirect(request.POST.get('redirect'))
+		else:
+			return render(request, 'auth/login.html', {'wrongPassword': True, 'redirect': redirect})
+	return render(request, 'auth/login.html', {'redirect': redirect})
+
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
+def register_view(response):
+	if response.method == "POST":
+		form = RegisterForm(response.POST)
+		if form.is_valid():
+			form.save()
+			return redirect("/chat")
+		else:
+			print(form.errors)  # Debugging: Ausgabe der Fehler im Formular
+			messages.error(response, 'There was an error in your registration form.')
+	else:
+		form = RegisterForm()
+
+	return render(response, "register/register.html", {"form": form})
+
+
+
+
--- first_django_app/settings.py
+++ first_django_app/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'chat',
+
+
 ]
 
 MIDDLEWARE = [
