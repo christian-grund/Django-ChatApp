9ae6225c2f4208b121dd4c2536b9fbafd1cf011f
christian-grund <151537557+christian-grund@users.noreply.github.com>
2024-07-11 16:23:03 +0200

    Updating API function
    and also the index(request) function

--- chat/views.py
+++ chat/views.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, JsonResponse
+from django.core import serializers
 from django.shortcuts import redirect, render
 from django.contrib import messages
 from django.contrib.auth import authenticate, login
@@ -6,6 +7,8 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from .forms import RegisterForm
 from .models import Chat, Message
+from django.http import HttpResponse
+from django.template import loader
 
 
 # request: Objekt, wird standardmäßig in diese Funktion reingegeben
@@ -14,7 +17,9 @@ def index(request):
 	if request.method == 'POST':
 		print("Received data: " + request.POST['textmessage'])
 		myChat = Chat.objects.get(id=1)
-		Message.objects.create(text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)  # Nachricht wird erstellt
+		new_message = Message.objects.create(text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)  # Nachricht wird erstellt
+		serialized_obj = serializers.serialize('json', [ new_message, ])
+		return JsonResponse(serialized_obj[1:-1], safe=False)
 	chatMessages = Message.objects.filter(chat__id=1)                            
 	return render(request, 'chat/index.html', {'messages': chatMessages})
 
@@ -48,6 +53,8 @@ def register_view(response):
 
 	return render(response, "register/register.html", {"form": form})
 
-
+def test_static(request):
+    template = loader.get_template('base.html')
+    return HttpResponse(template.render({}, request))
 
 
