5005c06ddee3334cf0821d592f8242ce121c28a0
christian-grund <151537557+christian-grund@users.noreply.github.com>
2024-07-18 08:59:25 +0200

    Sorted out some files

--- first_django_app/urls.py
+++ first_django_app/urls.py
@@ -21,7 +21,7 @@ from chat.views import index, login_view, logout_view, register_view, delete_mes
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', RedirectView.as_view(url='/login/', permanent=False)),
+    path('', RedirectView.as_view(url='/login/?next=/chat/', permanent=False)),
     path('chat/', index),
     path("register/", register_view, name='register'),
     path('login/', login_view, name='login'),
